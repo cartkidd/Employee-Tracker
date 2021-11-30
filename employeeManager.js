8335e787e2ef2214532b0dd2591c3cc7af22e8b8
Cartrell KIDD <cartrellk56@gmail.com>
2021-11-30 14:47:08 -0500

    finalized version

--- employeeManager.js
+++ employeeManager.js
@@ -1,11 +1,11 @@
 // List the dependencies here.
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const util = require('util');
 
 // Create the connection to MySQL WorkBench
-let connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
